32d1247e064e2c73a877b93ad0ae57dde370ff2c
Le Son Tung <tung.ls110106@gmail.com>
2026-05-19 23:54:10 +0700

    Reorganised project structure (mainly about view-UI) and maven config Fixed some issues about windows icon on other OS (Windows)

--- src/main/java/wildlife/core/Main.java
+++ src/main/java/wildlife/core/Main.java
@@ -1,7 +1,10 @@
 package wildlife.core;
-
+import wildlife.view.entry_point;
 public class Main {
     public static void main(String[] args) {
 
+
+        //Gọi cửa sổ giao diện
+        entry_point.main(args);
     }
 }
