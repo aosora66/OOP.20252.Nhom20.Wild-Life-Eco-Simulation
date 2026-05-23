50e042bd7f06c48ee3b6e24a2279dced71c8472f
Le Son Tung <tung.ls110106@gmail.com>
2026-05-23 16:54:31 +0700

    Finish basic UI components layout and some animation for 1920x1080 resolution. modified the maven build config for multiplatform binary applications release later.

--- src/main/java/wildlife/core/Main.java
+++ src/main/java/wildlife/core/Main.java
@@ -1,10 +1,16 @@
 package wildlife.core;
+import wildlife.view.Mobs;
 import wildlife.view.entry_point;
+
 public class Main {
     public static void main(String[] args) {
 
+        Mobs.MobsList.add(("vip1"));
+        Mobs.MobsList.add(("vip2"));
+        Mobs.MobsList.add(("vip3"));
+        Mobs.MobsList.add(("vip5"));
+        Mobs.MobsList.add(("vip6"));
 
-        //Gọi cửa sổ giao diện
         entry_point.main(args);
     }
 }
