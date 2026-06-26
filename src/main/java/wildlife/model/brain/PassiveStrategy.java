c1c6dcf93e415b38e57230b4512b309cd0f4bcdd
Truon Do Quokku Ain <tombraider@linux.com.vn>
2026-06-26 10:02:59 +0700

    moi 18 class diagram

--- src/main/java/wildlife/model/brain/PassiveStrategy.java
+++ src/main/java/wildlife/model/brain/PassiveStrategy.java
@@ -187,7 +187,6 @@ public class PassiveStrategy extends AbstractSurvivalStrategy {
             );
             wanderStepsLeft = WANDER_PERSIST_TICKS;
         }
-
         if (self.getPosition().distanceTo(wanderTarget) <= stepSize) {
             wanderStepsLeft = 0;
         } else {
